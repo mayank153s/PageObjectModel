6b7e1e0e3852977cfb3f8becbc11497a3ce12658
mayank <saxenamayank.153@gmail.com>
2025-06-21 20:56:36 +0530

    added table data fetching code

--- src/main/java/maven/abstractComponent/AbstractComponent.java
+++ src/main/java/maven/abstractComponent/AbstractComponent.java
@@ -5,12 +5,15 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AbstractComponent {
 
 	WebDriver driver;
+	
+
 	
 	public AbstractComponent(WebDriver driver) {
 		this.driver = driver;
@@ -28,5 +31,10 @@ public class AbstractComponent {
 		WebDriverWait ww = new WebDriverWait(driver,Duration.ofSeconds(20));
 		ww.until(ExpectedConditions.invisibilityOf(element));
 	}
+    
+    public void ScrollUntilELementFound(WebElement element) {
+    	Actions action = new Actions(driver);
+    	action.scrollToElement(element).perform();
+    }
 
 }
